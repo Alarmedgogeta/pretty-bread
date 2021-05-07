dd98b146f56f438ce1d3fcab021322d0937eb5a8
Jondragon77221155 <jondragon_m@yahoo.com.mx>
2021-05-07 16:11:14 -0500

    formato de not found

--- views/not_found.php
+++ views/not_found.php
@@ -1,8 +1,8 @@
 <div class="not_found">
   <div class="not_found__container">
     <div class="not_found__container--header">
-      <h1>Pagina no encontrada:c</h1>
-      <h2>F</h2>
+      <h1>Página no encontrada :c</h1>
+      <h2>Es necesario llevar realizar una revisión de la estructura de los directorios y su direccionamiento.</h2>
     </div>
   </div>
 </div>
