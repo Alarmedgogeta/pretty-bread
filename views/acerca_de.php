e918dd052343c88c6f1c6bb0b6c3b544c6b12cd9
Jondragon77221155 <jondragon_m@yahoo.com.mx>
2021-05-07 12:31:59 -0500

    Texto de layout "Acerca de "

--- views/acerca_de.php
+++ views/acerca_de.php
@@ -4,9 +4,9 @@
       <h1>Acerca de</h1>
     </div>
     <div class="main__container--content">
-      <p>All of our products are hand made by experienced bakers using only the best, natural ingredients available to us. When our products are packed, they are either delivered in recyclable cardboard, or individually wrapped for ‘grab and go’ in fully bio-degradable and home compostable responsibly sourced packaging.</p>
-      <p>All our products are made using flour from Wessex Mill, the small family run flour mill based in Wantage, Oxfordshire. They source and mill wheat from local farms within 30 miles of the mill. The finished flour is delivered to our bakery in West London, less than 50 miles away.</p>
-      <p>All our products freeze well, best frozen on the day of purchase. Defrost by leaving the product at ambient and then use as normal, alternatively check the product details on the website for the best options to refresh or heat up.</p>
+      <p>Este proyecto se ha creado con el fin de entender las Tecnologías de Construcción Web, para la construcción Full-Stack de Páginas Web.</p>
+      <p>El proyecto ha sido desarrollado por estudiantes de la Universidad del Valle de México, Campus Villa Hermosa y San Rafael.</p>
+      <p>Para su construcción se emplearon los lenguajes PHP, CSS, HTML y SQL.</p>
     </div>
   </div>
 </div>
