814a606988a141158c0c0a27300fb69a91239f8f
Alan Diaz Yañez <alandiazyanez@gmail.com>
2021-05-07 00:11:00 -0500

    Se hace la pagina de contacto y de acerda_de

--- views/contacto.php
+++ views/contacto.php
@@ -1,1 +1,59 @@
-<h1>Contacto</h1>
+<div class="main">
+  <div class="main__container">
+    <div class="main__container--title">
+      <h1>Contactanos</h1>
+    </div>
+    <div class="main__container--content">
+      <p>If you would like to stock our baked goods for your café, deli or shop or would like to use our products as ingredients in your restaurant, deli or café please contact us on:</p>
+      <div>
+        <span>020 8961 5931</span>
+        <span>orders@theartisanbakery.com</span>
+      </div>
+      <form action="/" method="post">
+        <div class="formulario--grupo">
+          <div class="formulario--input w-100">
+            <label for="nombre">Nombre:</label>
+            <input id="nombre" name="nombre" type="text" required />
+          </div>
+        </div>
+        <div class="formulario--grupo">
+          <div class="formulario--input w-50">
+            <label for="primer_apellido">Primer apellido:</label>
+            <input id="primer_apellido" name="primer_apellido" type="text" required />
+          </div>
+          <div class="formulario--input w-50">
+            <label for="segundo_apellido">Segundo apellido:</label>
+            <input id="segundo_apellido" name="segundo_apellido" type="text" required />
+          </div>
+        </div>
+        <div class="formulario--grupo">
+          <div class="formulario--input w-100">
+            <label for="email">Correo:</label>
+            <input id="email" name="email" type="text" required />
+          </div>
+        </div>
+        <div class="formulario--grupo">
+          <div class="formulario--input w-100">
+            <label for="tel">Telefono:</label>
+            <input id="tel" name="tel" type="text" required />
+          </div>
+        </div>
+        <div class="formulario--grupo">
+          <div class="formulario--input w-100">
+            <label for="asunto">Asunto:</label>
+            <input id="asunto" name="asunto" type="text" required />
+          </div>
+        </div>
+        <div class="formulario--grupo">
+          <div class="formulario--input w-100">
+            <label for="mensaje">Mensaje:</label>
+            <input id="mensaje" name="mensaje" type="text" required />
+          </div>
+        </div>
+        <div class="formulario--final">
+          <button class="boton" type="submit">Enviar</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
