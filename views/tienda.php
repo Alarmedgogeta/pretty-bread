fc5ba21c5453621b59c23a201f5e1e3980708864
Alan Diaz Yañez <alandiazyanez@gmail.com>
2021-05-07 01:10:06 -0500

    se agrega vista de la tienda

--- views/tienda.php
+++ views/tienda.php
@@ -1,1 +1,73 @@
-<h1>tienda</h1>
+<div class="main">
+  <div class="store__container">
+    <div class="products">
+      <div class="produtcs__container">
+        <div class="product_card">
+          <div class="product_card__container">
+            <div class="product_card__container--cover">
+              <img src="" alt="imagen-panecito" />
+            </div>
+            <div class="product_card__content">
+              <div class="product_card__content--name">
+                <span class="product-name">Cuernito</span>
+              </div>
+              <div class="product_card__content--price">
+                <span class="product-price">$ 599.00</span>
+              </div>
+              <div class="product_card__content--descripcion">
+                <span class="product-descripcion">Este es un cuernito bien bonito y sabrosito sdfasdfasfasfdasfasdf dasf sf af dsf dsa fas fasf sa fase fdes feas</span>
+              </div>
+              <div class="product_card__content--add">
+                <button class="btn-add">Agregar al carrito</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="product_card">
+          <div class="product_card__container">
+            <div class="product_card__container--cover">
+              <img src="" alt="imagen-panecito" />
+            </div>
+            <div class="product_card__content">
+              <div class="product_card__content--name">
+                <span class="product-name">Cuernito</span>
+              </div>
+              <div class="product_card__content--price">
+                <span class="product-price">$ 599.00</span>
+              </div>
+              <div class="product_card__content--descripcion">
+                <span class="product-descripcion">Este es un cuernito bien bonito y sabrosito sdfasdfasfasfdasfasdf dasf sf af dsf dsa fas fasf sa fase fdes feas</span>
+              </div>
+              <div class="product_card__content--add">
+                <button class="btn-add">Agregar al carrito</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="product_card">
+          <div class="product_card__container">
+            <div class="product_card__container--cover">
+              <img src="" alt="imagen-panecito" />
+            </div>
+            <div class="product_card__content">
+              <div class="product_card__content--name">
+                <span class="product-name">Cuernito</span>
+              </div>
+              <div class="product_card__content--price">
+                <span class="product-price">$ 599.00</span>
+              </div>
+              <div class="product_card__content--descripcion">
+                <span class="product-descripcion">Este es un cuernito bien bonito y sabrosito sdfasdfasfasfdasfasdf dasf sf af dsf dsa fas fasf sa fase fdes feas</span>
+              </div>
+              <div class="product_card__content--add">
+                <button class="btn-add">Agregar al carrito</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
