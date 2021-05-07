3ed8183da01da456442506211d33a7d607e0862b
Alfredo250195 <alfredo.cruces.ruiz@gmaii.com>
2021-05-07 13:21:38 -0500

    vacio de descripcion

--- views/tienda.php
+++ views/tienda.php
@@ -15,7 +15,7 @@
                 <span class="product-price">$ 599.00</span>
               </div>
               <div class="product_card__content--descripcion">
-                <span class="product-descripcion">Este es un cuernito bien bonito y sabrosito sdfasdfasfasfdasfasdf dasf sf af dsf dsa fas fasf sa fase fdes feas</span>
+                <span class="product-descripcion"></span>
               </div>
               <div class="product_card__content--add">
                 <button class="btn-add">Agregar al carrito</button>
