68c36c6683df2603df38d0aa049028bfa5f07dc7
Alan Diaz Yañez <alandiazyanez@gmail.com>
2021-05-07 00:18:08 -0500

    se agregan vista de login y registro

--- views/login.php
+++ views/login.php
@@ -1,1 +1,23 @@
-<h1>Iniciar sesion</h1>
+<div class="main">
+  <div class="main__container">
+    <div class="main__container--title">
+      <h1>Iniciar sesion</h1>
+    </div>
+    <div class="main__container--content">
+      <form action="/" method="post">
+        <div class="formulario--grupo">
+          <div class="formulario--input w-100">
+            <label for="email">Correo:</label>
+            <input id="email" name="email" type="text" required />
+          </div>
+        </div>
+        <div class="formulario--grupo">
+          <div class="formulario--input w-100">
+            <label for="pass">Contrasena:</label>
+            <input id="pass" name="pass" type="text" required />
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
--- views/registro.php
+++ views/registro.php
@@ -1,1 +1,44 @@
-<h1>Registrarme</h1>
+<div class="main">
+  <div class="main__container">
+    <div class="main__container--title">
+      <h1>Registrarme</h1>
+    </div>
+    <div class="main__container--content">
+      <form action="/" method="post">
+        <div class="formulario--grupo">
+          <div class="formulario--input w-100">
+            <label for="email">Correo:</label>
+            <input id="email" name="email" type="text" required />
+          </div>
+        </div>
+        <div class="formulario--grupo">
+          <div class="formulario--input w-100">
+            <label for="pass">Contrasena:</label>
+            <input id="pass" name="pass" type="text" required />
+          </div>
+        </div>
+        <div class="formulario--grupo">
+          <div class="formulario--input w-100">
+            <label for="nombre">Nombre:</label>
+            <input id="nombre" name="nombre" type="text" required />
+          </div>
+        </div>
+        <div class="formulario--grupo">
+          <div class="formulario--input w-100">
+            <label for="apellidos">Apellidos:</label>
+            <input id="apellidos" name="apellidos" type="text" required />
+          </div>
+        </div>
+        <div class="formulario--grupo">
+          <div class="formulario--input w-100">
+            <label for="tel">Telefono:</label>
+            <input id="tel" name="tel" type="text" required />
+          </div>
+        </div>
+        <div class="formulario--final">
+          <button class="boton" type="submit">Enviar</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
